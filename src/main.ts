e287b5830385cc5534ae98546634ab92828e6900
Andrea Locatelli <107633262+LocatAndre@users.noreply.github.com>
2025-03-15 13:20:00 +0100

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { bangMap } from './bang';
+import './style.css';
+
+function findBangAtEnd(inputString: string) {
+  const bangPattern = /!\w+$/;
+  const match = inputString.match(bangPattern);
+  return {
+    bang: match?.[0].replace('!', '') || 'g',
+    text: inputString.replace(match?.[0] || '', '').replace('/', ''),
+  };
+}
+
+export const startSearch = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get('q');
+  if (myParam) {
+    const { bang, text } = findBangAtEnd(myParam);
+    const selectedBang = bangMap.get(bang);
+    const urlString = selectedBang?.u.replace('{{{s}}}', text);
+    if (urlString) window.location.replace(urlString);
+  }
+};
+
+startSearch();
